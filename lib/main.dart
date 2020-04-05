8d882b5adb52205c66487daf1f7cb69b70389c97
Jānis <janis@grotus.lv>
2020-04-05 22:26:23 +0300

    added question list

--- lib/main.dart
+++ lib/main.dart
@@ -1,95 +1,138 @@
 import 'package:flutter/material.dart';
+import 'question.dart';
 
 void main() => runApp(Quizzler());
 
-class Quizzler extends StatelessWidget {
+class Quizzler extends StatefulWidget {
+  @override
+  _QuizzlerState createState() => _QuizzlerState();
+}
+
+class _QuizzlerState extends State<Quizzler> {
+  List<Icon> scoreKeeper = [];
+
+  List<Question> questionList = [
+    Question(
+        questionText: 'You can lead a cow down stairs but not up stairs.',
+        questionAnswer: false),
+    Question(
+        questionText:
+            'Approximately one quarter of human bones are in the feet.',
+        questionAnswer: true),
+    Question(
+      questionText: 'A slug\'s blood is green.',
+      questionAnswer: true,
+    ),
+  ];
+
+  var questionNumber = 0;
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        backgroundColor: Colors.grey.shade900,
-        body: SafeArea(
-          child: Padding(
-            padding: EdgeInsets.symmetric(horizontal: 10.0),
-            child: QuizPage(),
+      home: SafeArea(
+        child: Scaffold(
+          backgroundColor: Colors.black,
+          body: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              Expanded(
+                flex: 4,
+                child: Center(
+                  child: Text(
+                    questionList[questionNumber].questionText,
+                    //questions[questionNumber],
+                    style: TextStyle(color: Colors.white, fontSize: 20),
+                  ),
+                ),
+              ),
+              Expanded(
+                child: Padding(
+                  padding: const EdgeInsets.all(10.0),
+                  child: FlatButton(
+                    onPressed: () {
+                      setState(
+                        () {
+                          if (questionList[questionNumber].questionAnswer ==
+                              true) {
+                            print(questionList[questionNumber].questionAnswer);
+
+                            scoreKeeper.add(
+                              Icon(
+                                Icons.check,
+                                color: Colors.green,
+                              ),
+                            );
+                          } else {
+                            scoreKeeper.add(
+                              Icon(
+                                Icons.close,
+                                color: Colors.red,
+                              ),
+                            );
+                          }
+                          questionNumber++;
+                        },
+                      );
+                    },
+                    child: Text(
+                      'True',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 16,
+                      ),
+                    ),
+                    color: Colors.green,
+                  ),
+                ),
+              ),
+              Expanded(
+                child: Padding(
+                  padding: const EdgeInsets.all(10.0),
+                  child: FlatButton(
+                    onPressed: () {
+                      setState(
+                        () {
+                          if (questionList[questionNumber].questionAnswer ==
+                              false) {
+                            scoreKeeper.add(
+                              Icon(
+                                Icons.close,
+                                color: Colors.red,
+                              ),
+                            );
+                          } else {
+                            scoreKeeper.add(
+                              Icon(
+                                Icons.check,
+                                color: Colors.green,
+                              ),
+                            );
+                          }
+
+                          questionNumber++;
+                        },
+                      );
+                    },
+                    child: Text(
+                      'False',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 16,
+                      ),
+                    ),
+                    color: Colors.red,
+                  ),
+                ),
+              ),
+              Row(children: scoreKeeper)
+            ],
           ),
         ),
       ),
     );
   }
 }
-
-class QuizPage extends StatefulWidget {
-  @override
-  _QuizPageState createState() => _QuizPageState();
-}
-
-class _QuizPageState extends State<QuizPage> {
-  @override
-  Widget build(BuildContext context) {
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-      crossAxisAlignment: CrossAxisAlignment.stretch,
-      children: <Widget>[
-        Expanded(
-          flex: 5,
-          child: Padding(
-            padding: EdgeInsets.all(10.0),
-            child: Center(
-              child: Text(
-                'This is where the question text will go.',
-                textAlign: TextAlign.center,
-                style: TextStyle(
-                  fontSize: 25.0,
-                  color: Colors.white,
-                ),
-              ),
-            ),
-          ),
-        ),
-        Expanded(
-          child: Padding(
-            padding: EdgeInsets.all(15.0),
-            child: FlatButton(
-              textColor: Colors.white,
-              color: Colors.green,
-              child: Text(
-                'True',
-                style: TextStyle(
-                  color: Colors.white,
-                  fontSize: 20.0,
-                ),
-              ),
-              onPressed: () {
-                //The user picked true.
-              },
-            ),
-          ),
-        ),
-        Expanded(
-          child: Padding(
-            padding: EdgeInsets.all(15.0),
-            child: FlatButton(
-              color: Colors.red,
-              child: Text(
-                'False',
-                style: TextStyle(
-                  fontSize: 20.0,
-                  color: Colors.white,
-                ),
-              ),
-              onPressed: () {
-                //The user picked false.
-              },
-            ),
-          ),
-        ),
-        //TODO: Add a Row here as your score keeper
-      ],
-    );
-  }
-}
-
 /*
 question1: 'You can lead a cow down stairs but not up stairs.', false,
 question2: 'Approximately one quarter of human bones are in the feet.', true,
